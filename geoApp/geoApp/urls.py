453c205f260cb4c1f2221cda941722b84b8254f6
iamtekson <teksondada@gmail.com>
2021-02-02 22:34:36 +0700

    lat lng send to the databse

--- geoApp/geoApp/urls.py
+++ geoApp/geoApp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from shp.views import index
+from note.views import note
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('note/', note, name='note'),
 ]
