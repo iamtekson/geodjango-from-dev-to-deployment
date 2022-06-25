22e962cb82a3354dc710b1228a280d7bd656a426
Tek Kshetri <39838116+iamtekson@users.noreply.github.com>
2022-06-25 19:01:39 +0700

    Update models.py

--- geoApp/shp/models.py
+++ geoApp/shp/models.py
@@ -10,13 +10,21 @@ from sqlalchemy import *
 from geo.Geoserver import Geoserver
 from pg.pg import Pg
 
+####################################################################################
+# Please change the Pg parameters, Geoserver parameters and conn_str
+####################################################################################
 # initializing the library
 db = Pg(dbname='geoapp', user='postgres',
         password='gicait123', host='localhost', port='5432')
 geo = Geoserver('http://127.0.0.1:8080/geoserver',
                 username='admin', password='geoserver')
+# Database connection string (postgresql://${database_user}:${databse_password}@${database_host}:${database_port}/${database_name}
+conn_str = 'postgresql://postgres:gicait123@localhost:5432/geoapp'
 
 
+######################################################################################
+# Shp model
+######################################################################################
 # The shapefile model
 class Shp(models.Model):
     name = models.CharField(max_length=50)
@@ -27,7 +35,9 @@ class Shp(models.Model):
     def __str__(self):
         return self.name
 
-
+#########################################################################################
+# Django post save signal
+#########################################################################################
 @receiver(post_save, sender=Shp)
 def publish_data(sender, instance, created, **kwargs):
     file = instance.file.path
@@ -35,7 +45,6 @@ def publish_data(sender, instance, created, **kwargs):
     file_name = os.path.basename(file).split('.')[0]
     file_path = os.path.dirname(file)
     name = instance.name
-    conn_str = 'postgresql://postgres:gicait123@localhost:5432/geoapp'
 
     # extract zipfile
     with zipfile.ZipFile(file, 'r') as zip_ref:
@@ -76,7 +85,9 @@ def publish_data(sender, instance, created, **kwargs):
     geo.publish_style(
         layer_name=name, style_name='geoApp_shp', workspace='geoapp')
 
-
+#########################################################################################
+# Django post delete signal
+#########################################################################################
 @receiver(post_delete, sender=Shp)
 def delete_data(sender, instance, **kwargs):
     db.delete_table(instance.name, schema='data')
